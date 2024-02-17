05c444da70ed9d371b5a7f0816ff44ebd3d8bcfa
Tristan Ross <tristan.ross@midstall.com>
2024-02-16 18:25:34 -0800

    feat: make daemon build

--- build.zig
+++ build.zig
@@ -89,7 +89,6 @@ pub fn build(b: *std.Build) !void {
                 \\#define HAVE_APPARMOR
                 \\#define HAVE_APPARMOR_2_10
                 \\#define HAVE_LIBAUDIT
-                \\#define HAVE_SELINUX
                 \\#define DBUS_HAVE_LINUX_EPOLL
                 \\
             );
@@ -121,6 +120,7 @@ pub fn build(b: *std.Build) !void {
     libdbus.addCSourceFiles(.{
         .files = &.{
             dbusSource.path("dbus/dbus-address.c").getPath(dbusSource.builder),
+            dbusSource.path("dbus/dbus-asv-util.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-auth.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-bus.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-connection.c").getPath(dbusSource.builder),
@@ -137,13 +137,16 @@ pub fn build(b: *std.Build) !void {
             dbusSource.path("dbus/dbus-marshal-header.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-marshal-recursive.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-marshal-validate.c").getPath(dbusSource.builder),
+            dbusSource.path("dbus/dbus-mainloop.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-memory.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-mempool.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-message.c").getPath(dbusSource.builder),
+            dbusSource.path("dbus/dbus-message-util.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-misc.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-nonce.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-object-tree.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-pending-call.c").getPath(dbusSource.builder),
+            dbusSource.path("dbus/dbus-pollable-set.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-pipe.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-resources.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-server-debug-pipe.c").getPath(dbusSource.builder),
@@ -152,12 +155,16 @@ pub fn build(b: *std.Build) !void {
             dbusSource.path("dbus/dbus-sha.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-signature.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-string.c").getPath(dbusSource.builder),
+            dbusSource.path("dbus/dbus-string-util.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-syntax.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-sysdeps.c").getPath(dbusSource.builder),
+            dbusSource.path("dbus/dbus-sysdeps-util.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-threads.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-timeout.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-transport-socket.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-transport.c").getPath(dbusSource.builder),
+            dbusSource.path("dbus/dbus-userdb.c").getPath(dbusSource.builder),
+            dbusSource.path("dbus/dbus-userdb-util.c").getPath(dbusSource.builder),
             dbusSource.path("dbus/dbus-watch.c").getPath(dbusSource.builder),
         },
     });
@@ -181,12 +188,13 @@ pub fn build(b: *std.Build) !void {
             .files = &.{
                 dbusSource.path("dbus/dbus-uuidgen.c").getPath(dbusSource.builder),
                 dbusSource.path("dbus/dbus-server-unix.c").getPath(dbusSource.builder),
+                dbusSource.path("dbus/dbus-pollable-set-poll.c").getPath(dbusSource.builder),
+                dbusSource.path("dbus/dbus-pollable-set-epoll.c").getPath(dbusSource.builder),
                 dbusSource.path("dbus/dbus-file-unix.c").getPath(dbusSource.builder),
                 dbusSource.path("dbus/dbus-pipe-unix.c").getPath(dbusSource.builder),
                 dbusSource.path("dbus/dbus-sysdeps-pthread.c").getPath(dbusSource.builder),
                 dbusSource.path("dbus/dbus-sysdeps-util-unix.c").getPath(dbusSource.builder),
                 b.pathFromRoot("src/dbus-sysdeps-unix.c"),
-                dbusSource.path("dbus/dbus-userdb.c").getPath(dbusSource.builder),
                 dbusSource.path("dbus/dbus-transport-unix.c").getPath(dbusSource.builder),
             },
         });
@@ -236,6 +244,7 @@ pub fn build(b: *std.Build) !void {
     dbusDaemon.addIncludePath(configHeader.getDirectory());
     dbusDaemon.addIncludePath(dbusSource.path("."));
     dbusDaemon.linkLibrary(expat.artifact("expat"));
+    dbusDaemon.linkLibrary(libdbus);
 
     dbusDaemon.addCSourceFiles(.{
         .files = &.{
@@ -287,6 +296,12 @@ pub fn build(b: *std.Build) !void {
         });
 
         dbusDaemon.linkLibrary(libcap.artifact("cap-ng"));
+
+        dbusDaemon.addCSourceFiles(.{
+            .files = &.{
+                dbusSource.path("bus/dir-watch-inotify.c").getPath(dbusSource.builder),
+            },
+        });
     }
 
     b.installArtifact(dbusDaemon);
